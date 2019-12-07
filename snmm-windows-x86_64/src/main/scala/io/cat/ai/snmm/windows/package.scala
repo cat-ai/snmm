4459e4d35fad2f482a865925dfc2dd98278ab85b
Gadzhiev Kemran <k.gadzhiev@pflb.ru>
2019-12-07 17:00:25 +0300

    windows syntax for memoryapi object

--- snmm-windows-x86_64/src/main/scala/io/cat/ai/snmm/windows/package.scala
+++ snmm-windows-x86_64/src/main/scala/io/cat/ai/snmm/windows/package.scala
@@ -6,16 +6,18 @@ import io.cat.ai.snmm.windows.lib.WindowsMemoryApi
 
 package object windows {
 
+  import syntax._
+
   object memoryapi extends WindowsMemoryApi {
 
     /**
       * Maps a view of a file mapping into the address space of a calling process
       * */
-    def MapViewOfFile(hFileMappingObject: Pointer[Unit],
-                      dwDesiredAccess: Int,
-                      dwFileOffsetHigh: Int,
-                      dwFileOffsetLow: Int,
-                      dwNumberOfBytesToMap: Int = 0): Pointer[Unit] =
+    def MapViewOfFile(hFileMappingObject: HANDLE,
+                      dwDesiredAccess: DWORD,
+                      dwFileOffsetHigh: DWORD,
+                      dwFileOffsetLow: DWORD,
+                      dwNumberOfBytesToMap: SIZE_T = 0): LPVOID =
       PointerFactory {
         SnmmWindowsJni
           .WINDOWS_MEMORY_API
@@ -32,24 +34,24 @@ package object windows {
       * Locks the specified region of the process's virtual address space into physical memory, ensuring that subsequent access
       * to the region will not incur a page fault.
       * */
-    def VirtualLock(lpAddress: Pointer[Unit],
-                    dwSize: Int): Boolean =
+    def VirtualLock(lpAddress: LPVOID,
+                    dwSize: SIZE_T): BOOL =
       SnmmWindowsJni.WINDOWS_MEMORY_API.virtual_lock(lpAddress.address, dwSize)
 
     /**
       * Changes the protection on a region of committed pages in the virtual address space of the calling process
       * */
-    def VirtualProtect(lpAddress: Pointer[Unit],
-                       dwSize: Int,
-                       flNewProtect: Int,
-                       lpflOldProtect: Pointer[Int]): Boolean =
+    def VirtualProtect(lpAddress: LPVOID,
+                       dwSize: SIZE_T,
+                       flNewProtect: DWORD,
+                       lpflOldProtect: PDWORD): BOOL =
       SnmmWindowsJni.WINDOWS_MEMORY_API.virtual_protect(lpAddress.address, dwSize, flNewProtect, lpflOldProtect.address)
 
     /**
       * Writes to the disk a byte range within a mapped view of a file
       * */
-    def FlushViewOfFile(lpBaseAddress: Pointer[Unit],
-                        dwNumberOfBytesToFlush: Int): Boolean =
+    def FlushViewOfFile(lpBaseAddress: LPCVOID,
+                        dwNumberOfBytesToFlush: SIZE_T): BOOL =
       SnmmWindowsJni.WINDOWS_MEMORY_API.flush_view_of_file(lpBaseAddress.address, dwNumberOfBytesToFlush)
 
 
@@ -57,13 +59,13 @@ package object windows {
       * Unlocks a specified range of pages in the virtual address space of a process, enabling the system to swap the pages out
       * to the paging file if necessary
       * */
-    def VirtualUnlock(lpAddress: Pointer[Unit], dwSize: Int): Boolean =
+    def VirtualUnlock(lpAddress: LPVOID, dwSize: SIZE_T): BOOL =
       SnmmWindowsJni.WINDOWS_MEMORY_API.virtual_unlock(lpAddress.address, dwSize)
 
     /**
       * Unmaps a mapped view of a file from the calling process's address space
       * */
-    def UnmapViewOfFile(lpBaseAddress: Pointer[Unit]): Boolean =
+    def UnmapViewOfFile(lpBaseAddress: LPCVOID): BOOL =
       SnmmWindowsJni.WINDOWS_MEMORY_API.unmap_view_of_file(lpBaseAddress.address)
   }
 
